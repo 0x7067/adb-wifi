ade93113bac8d598ea81bc951a76f1034f7497f8
Pedro Guimarães <moisespedro15@gmail.com>
2018-03-05 14:35:20 -0300

    API level condition. Typo

--- adb-wifi.sh
+++ adb-wifi.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Restarting ADB
-echo Restaring ADB
+echo Restarting ADB
 adb kill-server >/dev/null
 adb start-server >/dev/null
 
 android_api_level=$(adb shell getprop ro.build.version.sdk)
 
-if [ "$android_api_level" -ge 24 ]; then
+if [ "$android_api_level" -ge 23 ]; then
        device_internal_ip=$(adb shell ip address show | grep wlan0 | sed -n 2p | awk '{print $2}' | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
 else
        device_internal_ip=$(adb shell netcfg | grep wlan0 | awk '{print $3}' | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
